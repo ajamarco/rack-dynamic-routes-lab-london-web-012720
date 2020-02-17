c466a418e21113e1b271027fbbaaa77fdd61815b
alexandre jamarco <alexandre.j@gmail.com>
2020-02-17 11:06:46 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,32 @@
+class Application
+ 
+    
+   
+    def call(env)
+      resp = Rack::Response.new
+      req = Rack::Request.new(env)
+   
+      if req.path.match(/items/)
+   
+        item_name = req.path.split("/items/").last #turn /songs/Sorry into Sorry
+        item = @@items.find{|i| i.name == item_name} 
+        
+        if !item
+            resp.status = 400 
+            resp.write "Item not found"  
+        else
+            resp.write item.price
+        end
+        
+
+      else
+        resp.write "Route not found"
+        resp.status = 404
+
+      end
+   
+      resp.finish
+    end
+  end
+  
+
